b167391dc93f2a16a7d30f572204414471af82ab
JonasKruckenberg <iterpre@protonmail.com>
2021-01-31 00:25:56 +0100

    chore: Update build chain

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,33 @@
-import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import markdown from 'vite-plugin-vuedoc'
+import { VitePWA } from 'vite-plugin-pwa'
+import imageset from '../vite-plugin-imageset'
 
-const config: UserConfig = {
-  sourcemap: true
-}
-export default config
+// https://vitejs.dev/config/
+export default defineConfig({
+  clearScreen: false,
+  plugins: [
+    markdown({}),
+    vue(),
+    {
+      ...imageset(),
+      enforce: 'pre',
+    },
+    {
+      ...VitePWA({
+        strategies: 'generateSW',
+        outDir: 'dist',
+      }),
+      enforce: 'post'
+    },
+  ],
+  // @ts-ignore
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify'
+  },
+  optimizeDeps: {
+    include: ['animejs/lib/anime.es.js']
+  }
+})
