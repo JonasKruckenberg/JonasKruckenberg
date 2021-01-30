9a6f97052f55982ee642909f3f6543a7023c85ec
JonasKruckenberg <iterpre@protonmail.com>
2021-01-31 00:30:14 +0100

    chore: include imageset plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import markdown from 'vite-plugin-vuedoc'
 import { VitePWA } from 'vite-plugin-pwa'
-import imageset from '../vite-plugin-imageset'
+import imageset from 'vite-plugin-imageset'
 
 // https://vitejs.dev/config/
 export default defineConfig({
