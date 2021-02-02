447834dabd4590914422b675ee86d4fa4f5f0a93
JonasKruckenberg <iterpre@protonmail.com>
2021-02-02 12:35:48 +0100

    feat: Add favicons & pwa icons

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,43 @@
   <router-view></router-view>
 </template>
 
+<script lang="ts" setup>
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "Portfolio | Jonas Kruckenberg",
+  meta: [
+    {
+      name: `description`,
+      content: `Im a datascience student and web3 contributor. Welcome to my portfolio!`,
+    },
+    {
+      name: "theme-color",
+      content: "#e63946",
+    },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      hef: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      type: "image/png",
+      href: "/apple-touch-icon-180x180.png",
+    },
+  ],
+});
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,54 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import markdown from 'vite-plugin-vuedoc'
 import { VitePWA } from 'vite-plugin-pwa'
-import imageset from 'vite-plugin-imageset'
+import imagetools from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   clearScreen: false,
   plugins: [
-    markdown({}),
     vue(),
-    {
-      ...imageset(),
-      enforce: 'pre',
-    },
-    {
-      ...VitePWA({
-        strategies: 'generateSW',
-        outDir: 'dist',
-      }),
-      enforce: 'post'
-    },
+    markdown({}),
+    imagetools(),
+    VitePWA({
+      manifest: {
+        short_name: 'Jonas Kruckenberg',
+        name: 'Jonas Kruckenberg | Portfolio',
+        background_color: '#333333',
+        theme_color: '#e63946',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable'
+          },
+          {
+            src: '/pwa-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      strategies: 'generateSW',
+      outDir: 'dist',
+    })
   ],
+  // @ts-ignore
   ssgOptions: {
     script: 'async',
     formatting: 'minify'
