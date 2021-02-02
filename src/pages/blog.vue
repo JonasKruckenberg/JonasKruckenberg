9a15cd26b5cb2a6ce2e3f8b17c8f317a9de6e215
JonasKruckenberg <iterpre@protonmail.com>
2021-02-03 00:43:24 +0100

    style: add blog styles

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,9 +1,58 @@
 <template>
-  <div class="user">
-    <h2>User {{ $route.params.title }}</h2>
+  <main>
     <router-view></router-view>
-  </div>
+  </main>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import "../assets/variables.scss";
+@import "../assets/code-theme-dark.scss";
+
+.markdown-body {
+  display: block;
+  margin: 32px;
+  padding: 2em;
+
+  table {
+    border-collapse: collapse;
+
+    th,
+    td {
+      padding: 12px;
+      text-align: left;
+    }
+    th {
+      background-color: $black;
+      color: $white;
+    }
+    th:first-child {
+      border-radius: $rounding 0 0 $rounding;
+    }
+    th:last-child {
+      border-radius: 0 $rounding $rounding 0;
+    }
+
+    tr:nth-child(even) {
+      background-color: #f2f2f2;
+    }
+  }
+
+  ul,
+  ol {
+    ::marker {
+      font-weight: $font-weight-bold;
+    }
+  }
+
+  blockquote {
+    padding: 20px;
+    border-left: 7px solid $red;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  img {
+    max-width: 33%;
+  }
+}
 </style>
