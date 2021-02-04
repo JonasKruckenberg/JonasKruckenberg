5fd753f0af8c754eedd14f123a55e26974502a02
JonasKruckenberg <iterpre@protonmail.com>
2021-02-04 17:52:17 +0100

    fix: import styles from correct place

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import Blog from './components/blog.vue'
 import routes from 'vite-plugin-pages/client';
-import './assets/master.scss'
+import './assets/styles/master.scss'
 
 export const createApp = ViteSSG(App, {
-    routes
+    routes,
+}, ({ app }) => {
+    app.component('blog', Blog)
 })
