e89f17482804031391fe5c76379e69064e9284bf
Jonas Kruckenberg <iterpre@protonmail.com>
2022-12-11 14:48:08 +0100

    inline critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 import compress from "astro-compress";
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jonaskruckenberg.de',
-  integrations: [mdx(), sitemap(), compress()]
+  integrations: [mdx(), sitemap(), critters(), compress()]
 });
