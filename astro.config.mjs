ae63dd7995e741cac15bb84a06b0b7f69a26148f
Jonas Kruckenberg <iterpre@protonmail.com>
2022-12-11 14:43:54 +0100

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://jonaskruckenberg.de',
-	integrations: [mdx(), sitemap()],
+  site: 'https://jonaskruckenberg.de',
+  integrations: [mdx(), sitemap(), compress()]
 });
