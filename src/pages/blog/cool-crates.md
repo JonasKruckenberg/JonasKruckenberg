14102e7a4ecf0a120f0fe0bfc4e8abb69e54c387
Jonas Kruckenberg <iterpre@protonmail.com>
2023-02-19 15:52:52 +0100

    add cool crates blog post

---
+++ src/pages/blog/cool-crates.md
@@ -0,0 +1,18 @@
+---
+layout: "../../layouts/BlogPost.astro"
+title: "Cool crates I like"
+description: ""
+pubDate: "Feb 04 2023"
+# heroImage: "/placeholder-hero.jpg"
+---
+
+## `fst`
+## `static-rc`
+## `ghost-cell`
+## `sycamore`
+## `heapless`
+## `snafu`
+## `blake3`
+## `bitflags`
+## `pretty_assertions`
+## `crossbeam`
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,26 @@
 import rss from '@astrojs/rss';
+import { MarkdownInstance } from 'astro'
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
+export const get = async () => {
+	const posts = import.meta.glob('./blog/**/*.{md,mdx}')
+
+	const items = await Promise.all(Object.values(posts).map(async (getPost) => {
+		const post = await getPost() as MarkdownInstance<{ title: string, description: string, pubDate: string }>;
+
+		return {
+			title: post.frontmatter.title,
+			description: post.frontmatter.description,
+			pubDate: new Date(post.frontmatter.pubDate),
+			content: post.compiledContent(),
+			link: post.url
+		}
+	}))	
+
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items
 	});
+}
