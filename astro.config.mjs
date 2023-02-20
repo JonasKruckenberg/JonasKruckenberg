93544c302f9141338563e8cd9b4c4102c4dcee0f
Jonas Kruckenberg <iterpre@protonmail.com>
2023-02-20 22:26:08 +0100

    feat: add katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,18 @@ import sitemap from '@astrojs/sitemap';
 import compress from "astro-compress";
 import critters from "astro-critters";
 import fs from 'fs'
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jonaskruckenberg.de',
   integrations: [svelte(), sitemap(), critters(), compress()],
   markdown: {
+    extendDefaultPlugins: true,
+    gfm: true,
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: JSON.parse(fs.readFileSync('./code-theme.json', 'utf-8'))
     }
